44e90632239063d041ce08185e987d68cfe8a9ae
sevdehilal <sevdedurak2002@gmail.com>
2024-08-29 14:26:52 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CountryList from './CountryList';
+import CountryDetails from './CountryDetails';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v2/all')
+      .then(response => setCountries(response.data))
+      .catch(error => console.log({ error }));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route 
+            path="/" 
+            element={<CountryList countries={countries} />} 
+          />
+          <Route 
+            path="/country/:code" 
+            element={<CountryDetails countries={countries} />} 
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
